d21b1eee4e3c704c21a38ccdd6732b6c6d3c53ad
YamatoDX <piyal@twurs.com>
2022-02-28 00:44:27 +0600

    another try

--- index.js
+++ index.js
@@ -9,49 +9,28 @@ app.use(express.json());
 
 const redisClient = Redis.createClient();
 
-app.get("/users", async (req, res) => {
-  try {
-    const x = redisClient.GET(
-      "responseData",
-      async (clientError, responseData) => {
-        console.log("i am inside");
-        if (clientError) {
-          console.log("client error");
-          return res.status(400).json({
-            message: "There was an error in redis client",
-            data: [],
-          });
-        }
-        if (responseData !== null) {
-          console.log("cached data", responseData);
-          return res.status(200).json({
-            data: responseData,
-          });
-        } else {
-          const response = await axios.get(
-            "https://jsonplaceholder.typicode.com/users"
-          );
-          const responseData = response.data;
-          // caching the data
-          redisClient.SETEX("responseData", 20, JSON.stringify(responseData));
-          return res.status(200).json({
-            data: responseData,
-            error: "",
-          });
-        }
-      }
+app.get("/users", (req, res) => {
+  redisClient.get("responseData", async (error, data) => {
+    if (error) {
+      console.error(err);
+      res.json({
+        data: err,
+      });
+    }
+    if (data !== null) {
+      res.json({
+        data,
+      });
+    }
+    const apiResponse = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
     );
-    return res.status(200).json({
-      x,
+    const apiData = apiResponse.data;
+    redisClient.setEx("responseData", 10, JSON.stringify(apiData));
+    res.json({
+      data: apiData,
     });
-  } catch (err) {
-    console.error("i was inside error");
-    console.error(err);
-    return res.status(400).json({
-      data: [],
-      error: err,
-    });
-  }
+  });
 });
 
 app.listen(4000, () => {
